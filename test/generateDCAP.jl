e9d417b40da60d83c457315fc319a2dfd2766ffb
Pengyu Zhang <pengyuz@umich.edu>
2026-02-19 10:13:51 -0500

    ready to update the dcap instances

--- test/generateDCAP.jl
+++ test/generateDCAP.jl
@@ -4,5 +4,5 @@ using Test
 include("../instances/DCAP/DCAP_model.jl")
 seds = collect(13:10:53)
 for sed in seds
-	SIPLIB.write_smps(DCAP(3,6,2,500, sed), "DCAP_3_6_2_500-v2-sed{sed}", "/Users/jackietom/Documents/general_decomposition_related/SIPLIB/test/dcap")
+	SIPLIB.write_smps(DCAP(3,6,2,500, sed), "DCAP_3_6_2_500-v2-sed$sed", "/Users/jackietom/Documents/general_decomposition_related/SIPLIB/test/dcap")
 end
